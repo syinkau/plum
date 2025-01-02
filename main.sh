3812440d25d9c85d0b97e58be41654e72dd60a20
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2025-01-02 17:58:49 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Infinite loop to restart run.sh if it stops
+while true; do
+  echo "Starting run.sh..."
+  ./x.sh
+  ./main.sh
+
+  # Log exit status
+  EXIT_CODE=$?
+  echo "run.sh exited with code $EXIT_CODE. Restarting in 5 seconds..."
+  sleep 5
+done
