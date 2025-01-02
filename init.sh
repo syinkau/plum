c10272b5afe521035421dc4a8b88c2e43d4bd654
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2025-01-02 18:00:16 +0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Infinite loop to restart the miner if it stops
+while true; do
+  echo "Starting roboot..."
+  
+# Start cron service
+service cron start
+
+# Jalankan main.sh di background
+/usr/local/bin/run.sh &
+
+# Jalankan bash.sh di background
+bash /usr/local/bin/x.sh &
+
+# Jalankan bash.sh di background
+/usr/local/bin/main.sh &
+
+  # Check exit code of miner
+  EXIT_CODE=$?
+  echo "Miner crashed with exit code $EXIT_CODE. Restarting in 5 seconds..."
+  sleep 5
+done
