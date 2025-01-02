f2219c6bd856be22d46348d640b7312f524ad8c9
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2025-01-02 17:57:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wallet and worker configuration
-WALLET="0x1bE17413356722a411033303EF7D8A13768fdF83"
+WALLET="0x37E9c5d313f890aC7Ed3841F91327B18259fdBF1"
 WORKER="linda"
 POOL_URL="stratum+tcp://$WALLET.$WORKER@pool-core-testnet.inichain.com:32672"
 
